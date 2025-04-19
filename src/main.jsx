d95345e635f3fab8662782b13afc219a9b69c414
swolekat <swolecattwitch@gmail.com>
2025-04-19 13:28:07 -0500

    get mantine in and start  thinking on stuff

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import '@mantine/core/styles.css';
 import App from './App.jsx'
+import {MantineProvider} from "@mantine/core";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <MantineProvider>
+          <App />
+      </MantineProvider>
   </StrictMode>,
 )
